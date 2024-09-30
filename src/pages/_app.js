d70c33e2b626c5eec8b666959c843cd0976cad24
yeontaekims <iioplk@jthefloor.co.kr>
2024-09-30 15:10:30 +0900

    meta 위치변경

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,9 @@
 import "@/styles/globals.css";
 import { useEffect } from "react";
 import "aos/dist/aos.css"; // AOS 스타일 불러오기
-
+import Head from "next/head";
 import AOS from "aos";
-// /** @type {import("next").Viewport} */
 
-// export const viewport = {
-//     width: "device-width",
-//     initialScale: 1,
-//     maximumScale: 1,
-//     userScalable: false,
-// };
 export default function App({ Component, pageProps }) {
     useEffect(() => {
         AOS.init({
@@ -22,6 +15,57 @@ export default function App({ Component, pageProps }) {
     }, []);
     return (
         <>
+            <Head>
+                <meta charset="utf-8" />
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1 ,maximum-scale=1.0, user-scalable=no"
+                />
+                <meta name="theme-color" content="#000000" />
+                <meta
+                    name="description"
+                    content="Web site created using create-react-app"
+                />
+                <title>길성재 ❤️ 정수진 결혼합니다.</title>
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:url"
+                    content="https://wedding.biggestpond.site/"
+                />
+                <meta
+                    property="og:title"
+                    content="길성재 ❤️ 정수진 결혼합니다."
+                />
+                <meta
+                    property="og:image"
+                    content="https://res.cloudinary.com/dqetywuo0/image/upload/v1727362236/KakaoTalk_20240926_234806628_f5kvfd.jpg"
+                />
+                <meta
+                    property="og:description"
+                    content="11월 3일 일요일 오후 12시 라비에벨웨딩홀"
+                />
+                <meta
+                    property="og:site_name"
+                    content="길성재 ❤️ 정수진 결혼합니다."
+                />
+                <meta property="og:locale" content="ko_KR" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <script
+                    src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+                    integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
+                    crossOrigin="anonymous"
+                    async
+                    defer
+                ></script>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                />
+            </Head>
             <Component {...pageProps} />;
         </>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ import tw, { styled } from "twin.macro";
 import { supabase } from "@/lib/initSupabase";
 import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { MapIcon } from "@heroicons/react/24/outline";
-import Head from "next/head";
+
 const Image_1 =
     "https://res.cloudinary.com/dqetywuo0/image/upload/v1726322692/image_1_ntrqlf.jpg";
 
@@ -342,57 +342,6 @@ export default function Home() {
     };
     return (
         <>
-            <Head>
-                <meta charset="utf-8" />
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1 ,maximum-scale=1.0, user-scalable=no"
-                />
-                <meta name="theme-color" content="#000000" />
-                <meta
-                    name="description"
-                    content="Web site created using create-react-app"
-                />
-                <title>길성재 ❤️ 정수진 결혼합니다.</title>
-                <meta property="og:type" content="website" />
-                <meta
-                    property="og:url"
-                    content="https://wedding.biggestpond.site/"
-                />
-                <meta
-                    property="og:title"
-                    content="길성재 ❤️ 정수진 결혼합니다."
-                />
-                <meta
-                    property="og:image"
-                    content="https://res.cloudinary.com/dqetywuo0/image/upload/v1727362236/KakaoTalk_20240926_234806628_f5kvfd.jpg"
-                />
-                <meta
-                    property="og:description"
-                    content="11월 3일 일요일 오후 12시 라비에벨웨딩홀"
-                />
-                <meta
-                    property="og:site_name"
-                    content="길성재 ❤️ 정수진 결혼합니다."
-                />
-                <meta property="og:locale" content="ko_KR" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <script
-                    src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
-                    integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
-                    crossOrigin="anonymous"
-                    async
-                    defer
-                ></script>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossorigin
-                />
-            </Head>
             <div className="bg_wrap">
                 <SnowAnimation />
             </div>
