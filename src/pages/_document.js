4861dd6989af19c407f4e674cddf33bec9d49536
yeontaekims <iioplk@jthefloor.co.kr>
2024-09-30 14:48:09 +0900

    viewport 설정_3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="ko">
-            <Head />
-
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Parisienne&display=swap"
+                    rel="stylesheet"
+                ></link>
+            </Head>
             <body className="antialiased">
                 <Main />
                 <NextScript />
